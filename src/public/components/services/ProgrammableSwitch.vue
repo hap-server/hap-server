40ceb5912a0a439e8bbcd522ea2f6f79bd63aba7
Samuel Elliott <samuel@fancy.org.uk>
2019-02-27 19:44:26 +0000

    Add programmable switches

--- src/public/components/services/ProgrammableSwitch.vue
+++ src/public/components/services/ProgrammableSwitch.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="service service-switch">
+    <div class="service service-switch" :class="{active}">
         <h5>{{ service.name || service.accessory.name }}</h5>
         <p>Programmable Switch</p>
+        <p v-if="last_event === 0">Single Press</p>
+        <p v-else-if="last_event === 1">Double Press</p>
+        <p v-else-if="last_event === 2">Long Press</p>
     </div>
 </template>
 
@@ -11,6 +14,50 @@
     export const uuid = Service.StatelessProgrammableSwitch;
 
     export default {
-        props: ['connection', 'service']
+        props: ['connection', 'service'],
+        data() {
+            return {
+                last_event: null,
+                active: false,
+            };
+        },
+        created() {
+            if (this.programmable_switch_event) {
+                this.programmable_switch_event.on('value-updated', this.handleSwitchEvent);
+            }
+        },
+        destroy() {
+            if (this.programmable_switch_event) {
+                this.programmable_switch_event.removeListener('value-updated', this.handleSwitchEvent);
+            }
+        },
+        computed: {
+            programmable_switch_event() {
+                return this.service.getCharacteristicByName('ProgrammableSwitchEvent');
+            }
+        },
+        watch: {
+            programmable_switch_event(programmable_switch_event, old_programmable_switch_event) {
+                if (old_programmable_switch_event) {
+                    this.programmable_switch_event.removeListener('value-updated', this.handleSwitchEvent);
+                }
+
+                if (programmable_switch_event) {
+                    programmable_switch_event.on('value-updated', this.handleSwitchEvent);
+                }
+            }
+        },
+        methods: {
+            handleSwitchEvent(value) {
+                //
+                console.log('Programmable Switch event', value);
+
+                this.last_event = value;
+                this.active = true;
+
+                setTimeout(() => this.active = false, 1000);
+                setTimeout(() => this.last_event = null, 5000);
+            }
+        }
     };
 </script>
