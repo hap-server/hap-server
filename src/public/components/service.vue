a20b73faf7f7aadadee49329ecae6431b270e782
Samuel Elliott <samuel@fancy.org.uk>
2019-03-02 09:09:08 +0000

    Hold service tiles to open details for mobile devices

--- src/public/components/service.vue
+++ src/public/components/service.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="service-wrapper" :data-accessory-uuid="service.accessory.uuid" :data-service-uuid="service.uuid" :data-service-type="service.type" @contextmenu.prevent="showDetails">
+    <div class="service-wrapper" :data-accessory-uuid="service.accessory.uuid" :data-service-uuid="service.uuid" :data-service-type="service.type" @contextmenu.prevent="showDetails" @touchstart="touchstart" @touchend="touchend">
         <component v-if="component" ref="service" :is="component" :class="{'details-open': details_open}" :connection="connection" :service="service" @show-details="this.$emit('show-details', () => details_open = false); details_open = true" />
 
         <div v-else-if="service.is_system_service" class="service unsupported-service error">
@@ -25,6 +25,7 @@
         data() {
             return {
                 details_open: false,
+                touchstart_timeout: null,
             };
         },
         computed: {
@@ -45,6 +46,16 @@
                     this.$emit('show-details', () => this.details_open = false);
                     this.details_open = true;
                 }
+            },
+            touchstart() {
+                if (this.touchstart_timeout) clearTimeout(this.touchstart_timeout);
+                this.touchstart_timeout = setTimeout(() => {
+                    this.touchstart_timeout = null;
+                    this.showDetails();
+                }, 500);
+            },
+            touchend() {
+                if (this.touchstart_timeout) clearTimeout(this.touchstart_timeout);
             }
         }
     };
