911dfe970280b7d4ee5a36bea9c21048e921eeec
Samuel Elliott <samuel@fancy.org.uk>
2019-03-15 22:11:15 +0000

    Add minified release build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,10 +2,15 @@ import gulp from 'gulp';
 import pump from 'pump';
 import babel from 'gulp-babel';
 import webpack from 'webpack-stream';
+import json from 'gulp-json-editor';
+import file from 'gulp-file';
+import minify from 'gulp-minify';
 
 import VueLoaderPlugin from 'vue-loader/lib/plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 
 const webpack_config = {
     mode: 'development',
@@ -99,8 +104,8 @@ gulp.task('watch-backend', gulp.series('build-backend', function () {
 
 gulp.task('watch-frontend', function () {
     return pump([
-        gulp.src('src/public/index.js'),
         gulp.src('src/public/scss/index.scss'),
+        gulp.src('src/public/index.js'),
         webpack(Object.assign({watch: true}, webpack_config)),
         gulp.dest('dist/public'),
     ]);
@@ -111,3 +116,87 @@ gulp.task('watch-example-plugins', gulp.series('build-example-plugins', function
 }));
 
 gulp.task('watch', gulp.parallel('watch-backend', 'watch-frontend', 'watch-example-plugins'));
+
+const release_minify_config = {
+    ext: {
+        min: '.js',
+    },
+    noSource: true,
+    mangle: {
+        keep_classnames: true,
+        module: true,
+    },
+};
+
+const release_webpack_config = Object.assign({}, webpack_config, {
+    mode: 'production',
+    output: undefined,
+    optimization: {
+        minimizer: [
+            new TerserPlugin(),
+            new OptimizeCSSAssetsPlugin(),
+        ],
+    },
+});
+
+gulp.task('build-backend-release', gulp.parallel(function () {
+    return pump([
+        gulp.src('src/index.js'),
+        gulp.src('src/cli.js'),
+        babel(),
+        minify(release_minify_config),
+        gulp.dest('release'),
+    ]);
+}, function () {
+    return pump([
+        gulp.src('src/core/**/*.js'),
+        babel(),
+        minify(release_minify_config),
+        gulp.dest('release/core'),
+    ]);
+}));
+
+gulp.task('build-frontend-release', function () {
+    return pump([
+        gulp.src('src/public/scss/index.scss'),
+        gulp.src('src/public/index.js'),
+        webpack(release_webpack_config),
+        gulp.dest('release/public'),
+    ]);
+});
+
+gulp.task('copy-release-executables', function () {
+    return pump([
+        file('hap-server',
+            `#!/usr/bin/env node\n` +
+            `process.title = 'hap-server';\n` +
+            `require('../cli');\n`, {src: true}),
+
+        gulp.dest('release/bin', {
+            mode: 0o755,
+        }),
+    ]);
+});
+
+gulp.task('copy-release-files', function () {
+    return pump([
+        gulp.src('package.json'),
+        json({
+            main: 'index.js',
+            files: [
+                'index.js',
+                'cli.js',
+            ],
+            devDependencies: [],
+            scripts: {
+                start: 'bin/hap-server',
+            },
+        }),
+
+        gulp.src('README.md'),
+
+        gulp.dest('release'),
+    ]);
+});
+
+gulp.task('build-release', gulp.parallel('build-backend-release', 'build-frontend-release', 'copy-release-executables', 'copy-release-files'));
