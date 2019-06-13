f2ffa50ef4b1b82bdf20e2dc27c3b75df1b8dfd2
Samuel Elliott <samuel@fancy.org.uk>
2019-06-13 17:55:38 +0100

    Example server plugin

---
+++ example-plugins/src/server-plugin/index.js
@@ -0,0 +1,21 @@
+import hapserver, {ServerPlugin, Events, EventListeners, log} from '@hap-server/api';
+
+hapserver.registerServerPlugin(class extends ServerPlugin {
+    async load() {
+        log.info('Loading server plugin', this.id, this.instance_id);
+
+        if (!this.listeners) this.listeners = new EventListeners();
+
+        // This is just an example of creating server plugins
+        // You could also listen on the global events object
+        this.server.on(Events.AddAccessoryEvent, event => {
+            log.withPrefix('ServerPlugin', this.id, this.instance_id).info('Added accessory %s to server', event.accessory.UUID);
+        }, this.listeners);
+    }
+
+    async unload() {
+        log.info('Unloading server plugin', this.id, this.instance_id);
+
+        this.listeners.cancel(true);
+    }
+});
