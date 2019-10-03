91859adfd8377e6eff0b73ba7f14f229e747954d
Samuel Elliott <samuel@fancy.org.uk>
2019-10-03 15:03:19 +0100

    Characteristic get/set handler types

--- src/server/hap-async.ts
+++ src/server/hap-async.ts
@@ -7,15 +7,35 @@
 import Logger from '../common/logger';
 import {Characteristic} from 'hap-nodejs';
 
+type GetCharacteristicHandler = (this: HAPNodeJS.Characteristic, context?: any, connection_id?: string) => Promise<any> | any;
+type SetCharacteristicHandler = (this: HAPNodeJS.Characteristic, new_value: any, context?: any, connection_id?: string) => Promise<void> | void;
+
+declare namespace HAPNodeJS {
+    interface Characteristic {
+        get_handler?: GetCharacteristicHandler;
+        getHandler(handler?: GetCharacteristicHandler): this;
+        // private _get_handler?: {
+        //     handler: GetCharacteristicHandler;
+        //     listener: (callback: (err, value) => void, context?, connection_id?: string) => void;
+        // };
+        set_handler?: SetCharacteristicHandler;
+        setHandler(handler?: SetCharacteristicHandler): this;
+        // private _set_handler?: {
+        //     handler: SetCharacteristicHandler;
+        //     listener: (new_value, callback: (err, value) => void, context?, connection_id?: string) => void;
+        // };
+    }
+}
+
 const log = new Logger('hap-async');
 
 Object.defineProperty(Characteristic.prototype, 'get_handler', {
     configurable: true,
     enumerable: true,
-    get() {
+    get(): GetCharacteristicHandler {
         return this._get_handler ? this._get_handler.handler : null;
     },
-    set(handler?: (context?, connection_id?) => Promise<any> | any) {
+    set(handler?: GetCharacteristicHandler) {
         if (this._get_handler) this.removeListener('get', this._get_handler.listener);
 
         if (handler) {
@@ -38,7 +58,7 @@ Object.defineProperty(Characteristic.prototype, 'get_handler', {
     },
 });
 
-Characteristic.prototype.getHandler = function(handler?: (context?, connection_id?) => Promise<any> | any) {
+(Characteristic.prototype as HAPNodeJS.Characteristic).getHandler = function(this: HAPNodeJS.Characteristic, handler?: GetCharacteristicHandler) {
     this.get_handler = handler;
     return this;
 };
@@ -49,7 +69,7 @@ Object.defineProperty(Characteristic.prototype, 'set_handler', {
     get() {
         return this._set_handler ? this._set_handler.handler : null;
     },
-    set(handler?: (new_value, context?, connection_id?) => Promise<void> | void) {
+    set(handler?: SetCharacteristicHandler) {
         if (this._set_handler) this.removeListener('set', this._set_handler.listener);
 
         if (handler) {
@@ -72,7 +92,7 @@ Object.defineProperty(Characteristic.prototype, 'set_handler', {
     },
 });
 
-Characteristic.prototype.setHandler = function(handler?: (new_value, context?, connection_id?) => Promise<void> | void) {
+(Characteristic.prototype as HAPNodeJS.Characteristic).setHandler = function(this: HAPNodeJS.Characteristic, handler?: SetCharacteristicHandler) {
     this.set_handler = handler;
     return this;
 };
