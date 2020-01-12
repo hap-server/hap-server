75f9066d6432ba4a7e295a24d7ba3da2d8cebab7
Samuel Elliott <samuel@fancy.org.uk>
2020-01-12 18:27:11 +0000

    Automatically resize terminal

--- src/public/components/terminal.vue
+++ src/public/components/terminal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div></div>
+    <div @resize="resize"></div>
 </template>
 
 <script>
@@ -10,7 +10,26 @@
             terminal: Terminal,
         },
         mounted() {
+            window.addEventListener('resize', this.resize);
+
             this.terminal.open(this.$el);
+            this.resize();
+        },
+        destroyed() {
+            window.removeEventListener('resize', this.resize);
+        },
+        methods: {
+            resize() {
+                const COLUMN_WIDTH = 7;
+                const ROW_HEIGHT = 14;
+
+                const cols = Math.floor(this.$el.clientWidth / COLUMN_WIDTH);
+                const rows = Math.floor((
+                    window.innerHeight - this.$el.offsetParent.offsetTop - this.$el.offsetTop - 48
+                ) / ROW_HEIGHT);
+
+                this.terminal.resize(cols, Math.max(rows, 24));
+            },
         },
     };
 </script>
